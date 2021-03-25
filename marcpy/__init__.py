05393eaf3d18e3fce5f643745e5f85abfa995ac2
jacpete <jacobmpeterson12@gmail.com>
2021-03-25 10:34:18 -0500

    add some helpful explanation comments to __init__.py

--- marcpy/__init__.py
+++ marcpy/__init__.py
@@ -1,3 +1,5 @@
+#Import submodules so that you can directly access them after importing the main package
 import marcpy.conda
 
+#Defines what supmodules should be imported when using $ from marcpy import *
 __all__ = ['conda']
