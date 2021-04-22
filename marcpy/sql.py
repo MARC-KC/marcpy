e5aaee0d22cfcb94c15e29d21883b4594f2bf191
jacpete <jpeterson@marc.org>
2021-04-22 09:02:30 -0500

    add getOBDCtable and _pandas_type_checker

--- marcpy/sql.py
+++ marcpy/sql.py
@@ -2,9 +2,12 @@
 functions may be hardcoded to work with Microsoft SQL Servers as that is what
 MARC uses.
 """
+import datetime
 
 import pyodbc
 from marcpy import keyring_wrappers
+import numpy
+import pandas
 
 def connectODBC(databaseString):
     """Connect to ODBC Database Using keyring
@@ -22,7 +25,7 @@ def connectODBC(databaseString):
 
     Returns
     -------
-    str
+    {pyodbc} connection
         A {pyodbc} connection object for the SQL Database.
     """
 
@@ -36,3 +39,101 @@ def connectODBC(databaseString):
     conn = pyodbc.connect(connString)
 
     return conn
+
+
+
+def _pandas_type_checker(type_in):
+    """Convert types to pandas types
+    
+    Uses a dictionary to return a compatible {pandas} datatype given a type 
+    input. This serves as an internal helper function for higher level 
+    functions in marcpy.
+    
+    Parameters
+    ----------
+    type_in : type
+        The type you want to convert to a pandas type.
+    
+    Return
+    ------
+    pandas type
+        A {pandas} compatible datatype. If the supplied type_in is not managed
+        in the type dictionary it will return None. If you come across this, you
+        can edit the dictionary in the function to handle the new type.
+    """
+    
+    switchDict = {
+        bool: "boolean",
+        int: "Int64",
+        float: "Float64",
+        str: "string",
+        datetime.date: pandas.DatetimeTZDtype,
+        datetime.datetime: pandas.DatetimeTZDtype
+    }
+    
+    pandasType = switchDict.get(type_in, None)    
+    return pandasType
+
+
+
+def getOBDCtable(conn, query):
+    """Get a {pandas} dataframe from the {pyodbc} connection
+    
+    This function is a more explicit implimentation of `pandas.read_sql()` when
+    working in an MSsql database. It builds up a dataframe using only the data
+    returned by the {pyodbc} cursor object. It was created so that columns get 
+    correctly typed without coercing too soon. The `pandas.read_sql()` function
+    defaults to the old {pandas} types prior to 1.0 (when the pandas.na) was 
+    introduced for backwards compatibility. This is made to correctly type 
+    data according to the new specifications.
+    
+    Parameters
+    ----------
+    conn : {pyodbc} connection
+        A {pyodbc} connection object for the SQL Database.
+    query : str
+        SQL Query to server to request table
+        
+    Return
+    ------
+    dataframe
+        A pandas dataframe with the query results.
+    """
+    
+    #Read from Database
+    cursor = conn.cursor()
+    cursor.execute(query)
+    names = [column[0] for column in cursor.description]
+    types = [column[1] for column in cursor.description]
+    nulls = [column[6] for column in cursor.description]
+    rows = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+
+
+    #Get Pandas types
+    pandasTypes = list(map(_pandas_type_checker, types))
+
+    #Make sure all types are managed by pandas_type_checker
+    if any(map(lambda x: x == None, pandasTypes)):
+        unmanagedIDs = numpy.where(numpy.array(list(map(lambda x: x == None, pandasTypes))))[0]
+        unmanagedNames = numpy.array(names)[unmanagedIDs]
+        unmanagedTypes = numpy.array(types)[unmanagedIDs]
+        message = "The following column(s) ['" + "', '".join(unmanagedNames) + "'] have the following unmanaged datatype(s) ['" + "', '".join(unmanagedTypes) + "'] for conversion to a pandas DataFrame."
+        message = message + "\nPlease edit `marcpy.pandas_type_checker()` to handle the unmanaged datatype(s)."
+        raise RuntimeError(message)
+
+
+    #Create from series
+    outSeries = [None] * len(names)
+    for i in range(0,len(names)):
+        colData = list(map(lambda x: x[i], rows))
+        colName = names[i]
+        colType = types[i]
+        colNull = nulls[i]
+        pandasType = pandasTypes[i]
+        outSeries[i] = pandas.Series(data = colData, dtype = pandasType, name = colName)
+
+    outPdf = pandas.concat(outSeries, axis = 1)
+
+    return outPdf
