d15cd30fb05c69da56e95fa91d0db66211ba5b43
jacpete <jpeterson@marc.org>
2021-07-14 01:24:57 -0500

    add sql alchemy to conda downloads

--- dev/PackageFunctionalityExamples.py
+++ dev/PackageFunctionalityExamples.py
@@ -1,4 +1,4 @@
-# conda install python=3.7 numpy pandas keyring pyodbc
+# conda install python=3.7 numpy pandas keyring pyodbc sqlalchemy
 # conda install arcpy --channel esri
 # pip install git+https://github.com/MARC-KC/marcpy
 # pip install -e L:\GitHub\marcpy
