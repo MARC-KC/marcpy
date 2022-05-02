bc46358abf05c8975cb807e3b3954874f6c18e9b
jacpete <jpeterson@marc.org>
2022-05-02 14:27:04 -0500

    add basic email module and function

---
+++ marcpy/email.py
@@ -0,0 +1,66 @@
+import os
+import zipfile
+import smtplib
+import email
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from marcpy.keyring_wrappers import key_get
+
+# fromEmail = "marc_gis@marc.org"
+# toEmails = 'jpeterson@marc.org'
+# subject = 'test email'
+# body = 'This is a test.\nThis is line2.'
+def sendEmail(fromEmail, toEmails, subject, body):
+     """Send Email
+    
+    Send an Email with Python.
+    This function is modified for Python 3 from an old local MARC script found 
+    in X:\Tools\Scripting\Admin\rsMailPython.py.
+    http://naelshiab.com/tutorial-send-email-python/
+    
+    Parameters
+    ----------
+    fromEmail : str
+        Email address you want to send from. it must be in your Window's 
+        Credential Manager as ':EMAIL:<fromEmail>'
+    toEmail : str, list
+        To what emails should the message be sent?
+    subject : str
+        Subject for email.
+    body : str
+        Body for email.
+        
+    Return
+    ------
+    None
+    """
+    
+    #Get stored email password
+    fromEmailPwd = key_get(serviceName="EMAIL", userName = fromEmail)
+    
+    #Set up smtplib object
+    s = smtplib.SMTP('smtp.office365.com')
+    s.starttls()
+    s.login(fromEmail, fromEmailPwd)
+    
+    #set up message precursers
+    sender = fromEmail
+    #Enforce to is a list
+    if isinstance(toEmails, str):
+        toEmails = [toEmails]
+    recipients = toEmails
+    body = body
+    
+    #Create message object
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ", ".join(toEmails)
+    msg.attach(MIMEText(body,'plain'))
+    
+    #Send email
+    s.sendmail(sender, recipients, msg.as_string())
+    
+    #Close object
+    s.quit()
