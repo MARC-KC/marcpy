b2f60de4dbf401164b8391d7938945cf88790228
jacpete <jpeterson@marc.org>
2022-05-26 13:19:01 -0500

    remove getODBCtable from init module

--- marcpy/__init__.py
+++ marcpy/__init__.py
@@ -15,7 +15,6 @@ from marcpy.keyring_wrappers import key_get
 from marcpy.keyring_wrappers import key_delete
 
 from marcpy.sql import connectODBC
-from marcpy.sql import getOBDCtable
 from marcpy.anti_join import anti_join
 
 #Defines what supmodules should be imported when using $ from marcpy import *
